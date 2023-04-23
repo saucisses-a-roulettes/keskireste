f358652a6e03e18a5cd58fb68a401ca3189d7e4f
Saucisse à roulettes <gaelmonach123@live.fr>
2023-04-23 11:07:28 +0200

    feat(BudgetChartView): and in progress BudgetChartView
    Signed-off-by: Saucisse à roulettes <gaelmonach123@live.fr>

--- src/infrastructure/pyside/dashboard.py
+++ src/infrastructure/pyside/dashboard.py
@@ -17,8 +17,8 @@
 import datetime
 from collections.abc import Mapping
 
-from PySide6.QtCharts import QChart, QBarSet, QBarSeries, QChartView, QBarCategoryAxis, QValueAxis
-from PySide6.QtCore import Qt
+from PySide6.QtCharts import QChart, QBarSet, QBarSeries, QChartView, QBarCategoryAxis, QValueAxis, QLineSeries
+from PySide6.QtCore import Qt, QPoint
 from PySide6.QtGui import QPainter
 from PySide6.QtWidgets import QWidget, QVBoxLayout
 
@@ -38,18 +38,25 @@ class BudgetChartView(QChartView):
         self._chart = QChart()
         self._bar_series = QBarSeries()
         self._chart.addSeries(self._bar_series)
+        self._line_series = QLineSeries()
+        self._line_series.setName("Economies")
+        self._chart.addSeries(self._line_series)
         self._chart.setTitle("Balance")
+        self._chart.setAnimationOptions(QChart.SeriesAnimations)  # type: ignore
 
-        categories = [f"{m}" for m in range(12, 1)]
+        categories = [f"{m}" for m in range(1, 13)]
         self._axis_x = QBarCategoryAxis()
         self._axis_x.append(categories)
         self._axis_x.setRange("1", "12")
         self._chart.addAxis(self._axis_x, Qt.AlignBottom)
         self._bar_series.attachAxis(self._axis_x)
+        self._line_series.attachAxis(self._axis_x)
 
         self._axis_y = QValueAxis()
+        self._axis_y.setTickCount(20)
         self._chart.addAxis(self._axis_y, Qt.AlignLeft)
         self._bar_series.attachAxis(self._axis_y)
+        self._line_series.attachAxis(self._axis_y)
         self._chart.legend().setVisible(True)
         self._chart.legend().setAlignment(Qt.AlignBottom)
 
@@ -57,24 +64,31 @@ class BudgetChartView(QChartView):
         self.setRenderHint(QPainter.Antialiasing)
 
     def refresh(self, year: int, histories: list[HistoryReadResponse]) -> None:
-        series = self._chart.series()[0]
+        self._bar_series.clear()
 
-        if isinstance(series, QBarSeries):
-            series.clear()
+        month_history_mapping: Mapping[int, History | None] = {
+            m: next((h for h in histories if h.date.year == year and h.date.month == m), None) for m in range(1, 13)
+        }
 
-            month_history_mapping: Mapping[int, History | None] = {
-                m: next((h for h in histories if h.date.year == year and h.date.month == m), None) for m in range(1, 13)
-            }
+        balance_data_set = QBarSet("Balance")
+        balances = [h.balance if h else 0 for m, h in month_history_mapping.items()]
+        balance_data_set.append(balances)
 
-            data_set = QBarSet("Balance")
-            data_set.append([h.balance if h else 0 for m, h in month_history_mapping.items()])
+        self._bar_series.append(balance_data_set)
 
-            series.append(data_set)
+        self._line_series.clear()
+        v: float = 0
+        economies = []
+        for i, b in enumerate(balances):
+            v += b or 0
+            economies.append(v)
+            self._line_series.append(QPoint(i, int(v)))
 
-            self._chart.update()
+        self._axis_y.setRange(min(balances + economies), max(balances + economies))
+        self._chart.update()
 
 
-class BudgetChartView(QWidget):
+class BudgetDashboardWidget(QWidget):
     def __init__(
         self, budget_reader: BudgetReader, history_reader: HistoryReader, parent: QWidget | None = None
     ) -> None:
