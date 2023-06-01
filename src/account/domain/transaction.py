86320b8644517284ed952f9ca6fd176ed9f37081
Saucisse à roulettes <gaelmonach123@live.fr>
2023-06-01 14:49:41 +0200

    KES-2-Transaction-Aggregate (#27)
    * add(transaction): file
    
    Signed-off-by: Saucisse à roulettes <gaelmonach123@live.fr>
    
    * impl(transaction): and tests
    
    Signed-off-by: Saucisse à roulettes <gaelmonach123@live.fr>
    
    * fix(transaction): date return type
    
    Signed-off-by: Saucisse à roulettes <gaelmonach123@live.fr>
    
    ---------
    
    Signed-off-by: Saucisse à roulettes <gaelmonach123@live.fr>

---
+++ src/account/domain/transaction.py
@@ -0,0 +1,54 @@
+#  /*
+#   * Copyright (c) 2023 Gael Monachon
+#   *
+#   * This program is free software: you can redistribute it and/or modify
+#   * it under the terms of the GNU General Public License as published by
+#   * the Free Software Foundation, either version 3 of the License, or
+#   * (at your option) any later version.
+#   *
+#   * This program is distributed in the hope that it will be useful,
+#   * but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   * GNU General Public License for more details.
+#   *
+#   * You should have received a copy of the GNU General Public License
+#   * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#   */
+import datetime
+
+from src.account.domain.account import UserId
+from src.shared.domain.entity import EntityBase, TId
+
+
+class Transaction(EntityBase[TId]):
+    def __init__(self, id_: TId, user_id: UserId, date: datetime.date, label: str, amount: float) -> None:
+        super().__init__(id_)
+        self._user_id = user_id
+        self._date = date
+        self._label = label
+        self._amount = amount
+
+    @property
+    def user_id(self) -> UserId:
+        return self._user_id
+
+    @property
+    def date(self) -> datetime.date:
+        return self._date
+
+    @property
+    def label(self) -> str:
+        return self._label
+
+    @property
+    def amount(self) -> float:
+        return self._amount
+
+    def rectify_date(self, new_date: datetime.date) -> None:
+        self._date = new_date
+
+    def rectify_amount(self, new_amount: float) -> None:
+        self._amount = new_amount
+
+    def modify_label(self, new_label: str) -> None:
+        self._label = new_label
