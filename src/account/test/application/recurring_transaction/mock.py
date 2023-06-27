14cd4d2f0b75b7509eeadba7c13ba231678042b2
JVGRVS <grob.julien1@gmail.com>
2023-06-27 16:11:22 +0200

    feat(recurring_transaction): implement recurring transaction creator and test

---
+++ src/account/test/application/recurring_transaction/mock.py
@@ -0,0 +1,25 @@
+#  /*
+#   * Copyright (c) 2023 Gael Monachon
+#   *
+#   * This program is free software: you can redistribute it and/or modify
+#   * it under the terms of the GNU General Public License as published by
+#   * the Free Software Foundation, either version 3 of the License, or
+#   * (at your option) any later version.
+#   *
+#   * This program is distributed in the hope that it will be useful,
+#   * but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   * GNU General Public License for more details.
+#   *
+#   * You should have received a copy of the GNU General Public License
+#   * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#   */
+import ppa
+
+from src.account.application.reccurring_transaction.repository import RecurringTransactionRepository
+from src.shared.application.repository import EntityAlreadyExists
+
+
+@ppa.in_memory_repository(entity_already_exists_exception=EntityAlreadyExists)
+class RecurringTransactionMockRepository(RecurringTransactionRepository):
+    pass
