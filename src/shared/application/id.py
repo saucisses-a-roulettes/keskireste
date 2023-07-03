fb4a874b6d62b0130c3b4e7908bd2f6cc9c1fd27
Saucisse à roulettes <gaelmonach123@live.fr>
2023-07-03 18:36:01 +0200

    feat(global): implement IdFactory interface

---
+++ src/shared/application/id.py
@@ -0,0 +1,26 @@
+#  /*
+#   * Copyright (c) 2023 Gael Monachon
+#   *
+#   * This program is free software: you can redistribute it and/or modify
+#   * it under the terms of the GNU General Public License as published by
+#   * the Free Software Foundation, either version 3 of the License, or
+#   * (at your option) any later version.
+#   *
+#   * This program is distributed in the hope that it will be useful,
+#   * but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   * GNU General Public License for more details.
+#   *
+#   * You should have received a copy of the GNU General Public License
+#   * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#   */
+from abc import ABC, abstractmethod
+from typing import Generic
+
+from src.shared.domain.entity import TId
+
+
+class IdFactory(ABC, Generic[TId]):
+    @abstractmethod
+    def generate_id(self) -> TId:
+        pass
