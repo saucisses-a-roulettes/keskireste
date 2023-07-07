9c5f545506e3738009b0268257bbdc00372f33c5
Saucisse à roulettes <gaelmonach123@live.fr>
2023-07-08 00:00:34 +0200

    feat(user): implement email_address PUT endpoint

--- src/infrastructure/user/fastapi/email_address.py
+++ src/infrastructure/user/fastapi/email_address.py
@@ -18,8 +18,11 @@ from dependency_injector.wiring import inject, Provide
 from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 
+from src.application.user.email_address.modifier import UserEmailAddressModifier, UserEmailAddressModificationRequest
 from src.application.user.subscription.email_address.validator import EmailAddressValidator
+from src.application.user.subscription.emailer import ValidationEmailSender
 from src.infrastructure.containers.in_memory import InMemoryContainer
+from src.infrastructure.user.id import UserUUID
 from src.shared.domain.email import EmailAddress
 
 router = APIRouter()
@@ -29,10 +32,32 @@ class _EmailAddressValidationBody(BaseModel):
     email_address: str
 
 
-@router.post("/validation", status_code=200)
+@router.post("/user/email_address/validation", status_code=200)
 @inject
 async def submit_user_email_address_validation(
     body: _EmailAddressValidationBody,
     email_address_validator: EmailAddressValidator = Depends(Provide[InMemoryContainer.email_address_validator]),
 ) -> None:
     email_address_validator.query(EmailAddress(body.email_address))
+
+
+class _EmailAddressModificationBody(BaseModel):
+    email_address: str
+    validation_token: str
+
+
+@router.put("/user/{user_id}/email_address", status_code=200)
+@inject
+async def modify_user_email_address(
+    user_id: str,
+    body: _EmailAddressModificationBody,
+    validation_email_sender: ValidationEmailSender = Depends(Provide[InMemoryContainer.validation_email_sender]),
+    user_email_address_modifier: UserEmailAddressModifier = Depends(
+        Provide[InMemoryContainer.user_email_address_modifier]
+    ),
+) -> None:
+    email_address = EmailAddress(body.email_address)
+    validation_email_sender.check_validation_token(email_address, body.validation_token)
+    user_email_address_modifier.modify(
+        UserEmailAddressModificationRequest(UserUUID(user_id), email_address=email_address)
+    )
