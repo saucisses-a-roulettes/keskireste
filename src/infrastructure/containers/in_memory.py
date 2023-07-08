7351000018ca126580e068d43f5f90a44fa92f0a
Saucisse à roulettes <gaelmonach123@live.fr>
2023-07-08 11:06:56 +0200

    feat(user): implement GET endpoint

--- src/infrastructure/containers/in_memory.py
+++ src/infrastructure/containers/in_memory.py
@@ -21,6 +21,7 @@ from src.application.account.creator import AccountCreator
 from src.application.user.creator import UserCreator
 from src.application.user.deleter import UserDeleter
 from src.application.user.email_address.modifier import UserEmailAddressModifier
+from src.application.user.reader import UserReader
 from src.application.user.subscription.email_address.validator import EmailAddressValidator
 from src.application.user.updater import UserUpdater
 from src.test.application.account.mock import AccountMockRepository, MockAccountIdFactory
@@ -36,6 +37,7 @@ class InMemoryContainer(DeclarativeContainer):
     user_creator = Factory(UserCreator, repository=user_repository, id_factory=user_id_factory)
     user_updater = Factory(UserUpdater, repository=user_repository)
     user_deleter = Factory(UserDeleter, repository=user_repository)
+    user_reader = Factory(UserReader, repository=user_repository)
     user_password_vault = Factory(UserPasswordVaultMock)
     user_email_address_modifier = Factory(UserEmailAddressModifier, repository=user_repository)
 
